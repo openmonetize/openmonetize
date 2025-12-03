c1604c9c1658cc15417e16fc4c7d432fceb864a1
openmonetize <gamaprojector@gmail.com>
2025-12-03 17:51:26 +0200

    chore: configure commitlint subject casing and full stop rules, and remove husky script boilerplate

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+        'subject-case': [2, 'always', ['sentence-case', 'start-case', 'pascal-case', 'upper-case', 'lower-case']],
+        'subject-full-stop': [0, 'never'],
+    },
 };
