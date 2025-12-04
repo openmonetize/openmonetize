509d81a3902482e5c01a73650ac6acfce0fbe86b
openmonetize <gamaprojector@gmail.com>
2025-12-04 11:26:11 +0200

    chore: bump SDK version from 0.6.0 to 0.6.1.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'subject-case': [2, 'always', ['sentence-case', 'start-case', 'pascal-case', 'upper-case', 'lower-case']],
+        'subject-case': [0],
         'subject-full-stop': [0, 'never'],
         'header-max-length': [2, 'always', 160],
     },
